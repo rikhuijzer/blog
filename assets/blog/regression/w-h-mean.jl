ad13cc9888f7319307f586ee2e15eab1fab59d28
rikhuijzer <rikhuijzer@users.noreply.github.com>
2020-10-21 14:57:37 +0000

    deploy: ea319258371728ce7d60c6c6e8eee7d05b0df5e1

--- assets/blog/regression/w-h-mean.jl
+++ assets/blog/regression/w-h-mean.jl
@@ -1,15 +1,9 @@
 # This file was generated, do not modify it. # hide
-# hideall
-
 m = mean(H) 
 sum_sq = r2(sum((H .- m).^2))
-
-function plot_lsq()
-  p = plot(df, x = :W, y = :H,
-    Geom.point,
-    yintercept = [m], Geom.hline(style = :dot)
-  ) 
-  p |> SVG(joinpath(@OUTPUT, "w-h-mean.svg")) # hide 
-end
-
-plot_lsq()
+p = plot(df, x = :W, y = :H,
+  Geom.point,
+  yintercept = [m], Geom.hline(style = :dot),
+  xs = [1, 2], ys = [2, 3], Shape.vline()
+)
+p |> SVG(joinpath(@OUTPUT, "w-h-mean.svg")) # hide
