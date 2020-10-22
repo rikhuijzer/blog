8c83d6dc2252522b35cf670ea83d728d64f835e7
rikhuijzer <rikhuijzer@users.noreply.github.com>
2020-10-22 14:04:27 +0000

    deploy: 6024a54aa90d46d5d236828e0b1a09efcedf5bf5

--- assets/posts/correlations/mycov.jl
+++ assets/posts/correlations/mycov.jl
@@ -1,7 +1,7 @@
 # This file was generated, do not modify it. # hide
 using Statistics
 
-# Note that unlike the Julia built-in function this function does not apply Bessel's correction.
+# Note that this function does not apply Bessel's correction.
 function mycov(X, Y)
     min_mean_x(x)::Float64 = x - mean(X)
     min_mean_y(y)::Float64 = y - mean(Y)
