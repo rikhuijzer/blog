d1585d9fa010818a3d8ba8b56ea7afc525f45672
Rik Huijzer <t.h.huijzer@rug.nl>
2021-02-11 18:07:37 +0100

    Fix missing image

--- _literate/random-forest.jl
+++ _literate/random-forest.jl
@@ -31,7 +31,7 @@ first(df, 10)
 
 #
 
-write_svg(name, p) = draw(SVG(joinpath(tempdir(), "$name.svg")), p) # hide
+write_svg(name, p) = draw(SVG(joinpath(@OUTPUT, "$name.svg")), p) # hide
 write_svg("u-class", # hide
 plot(df, x = :U, y = :V, color = :class)
 ); # hide
