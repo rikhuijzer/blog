ee9295504c0346b45289647eaa8714482c9ed93e
rikhuijzer <rikhuijzer@users.noreply.github.com>
2021-02-17 20:12:23 +0000

    deploy: 4bcc96a99b059e3dbd2b5d2ae5e31d3a300ca76c

--- assets/index/basic-plot.jl
+++ assets/index/basic-plot.jl
@@ -3,7 +3,7 @@ using Gadfly
 output_dir = @OUTPUT # hide 
 write_svg(name, p) = draw(SVG(joinpath(output_dir, "$name.svg")), p) # hide
 
-range = 1:30
+X = 1:30
 write_svg("basic-plot", # hide
-plot(x = range, y = [x^2 for x in range]) 
+plot(x = X, y = [x^2 for x in X]) 
 ) # hide
