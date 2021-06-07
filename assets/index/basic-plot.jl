1509468de5bb85473c22192657b24fb5a3a3336f
rikhuijzer <rikhuijzer@users.noreply.github.com>
2021-06-07 20:25:15 +0000

    deploy: 32c1c31ec6b3bcbdba4fa7edf7d00d01e62b2283

--- assets/index/basic-plot.jl
+++ assets/index/basic-plot.jl
@@ -1,9 +1,12 @@
 # This file was generated, do not modify it. # hide
-using Gadfly
-output_dir = @OUTPUT # hide 
-write_svg(name, p) = draw(SVG(joinpath(output_dir, "$name.svg")), p) # hide
+using AlgebraOfGraphics, CairoMakie
+output_dir = @OUTPUT # hide
+write_svg(name, fg) = save(joinpath(@OUTPUT, "$name.svg"), fg) # hide
 
 X = 1:30
+df = (x=X, y=X.*2)
+xy = data(df) * mapping(:x, :y)
+
 write_svg("basic-plot", # hide
-plot(x = X, y = X.^2) 
+draw(xy)
 ) # hide
