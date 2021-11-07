8e922e98986edcea2f37d4b2862e00ab98022407
Rik Huijzer <rikhuijzer@pm.me>
2021-11-07 18:33:25 +0100

    Go back to Franklin 0.10.37

--- utils.jl
+++ utils.jl
@@ -25,7 +25,7 @@ end
 Plug in the list of blog posts contained in the `/posts` folder.
 Souce: <https://github.com/abhishalya/abhishalya.github.io>.
 """
-@delay function hfun_blogposts()
+function hfun_blogposts()
     today = Dates.today()
     curyear = year(today)
     curmonth = month(today)
